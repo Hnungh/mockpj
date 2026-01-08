03ce5bb911c11e05c1664f68875823a0873d6862
Hnungh <hnunghnoy1@gmail.com>
2026-01-08 12:47:31 +0700

    Update khqr.js

--- khqr.js
+++ khqr.js
@@ -17,7 +17,7 @@ function crc16(payload) {
 /*
  ⚠️ CHANGE HERE ให้ตรง ABA ของคุณ
 */
-const ABA_ACCOUNT = "013946223"; // เบอร์ ABA / Account จริง
+const ABA_ACCOUNT = "4942576"; // เบอร์ ABA / Account จริง
 const MERCHANT_NAME = "BARDS";
 const MERCHANT_CITY = "PHNOM PENH";
 
