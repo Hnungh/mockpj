2288a2bb469f314f56c7125677261ea41e21ba4d
Hnungh <hnunghnoy1@gmail.com>
2026-01-08 13:13:02 +0700

    Update khqr.js

--- khqr.js
+++ khqr.js
@@ -1,30 +1,32 @@
-<script>
 function crc16(payload){
   let crc = 0xFFFF;
-  for (let c of payload){
-    crc ^= c.charCodeAt(0)<<8;
-    for(let i=0;i<8;i++)
-      crc = (crc&0x8000)?(crc<<1)^0x1021:crc<<1;
+  for (let i = 0; i < payload.length; i++) {
+    crc ^= payload.charCodeAt(i) << 8;
+    for (let j = 0; j < 8; j++) {
+      crc = (crc & 0x8000) ? (crc << 1) ^ 0x1021 : crc << 1;
+    }
   }
-  return ("0000"+(crc&0xFFFF).toString(16).toUpperCase()).slice(-4);
+  return (crc & 0xFFFF).toString(16).toUpperCase().padStart(4,'0');
 }
 
-function tag(id,val){
-  return id + val.length.toString().padStart(2,'0') + val;
+function tag(id, value){
+  return id + value.length.toString().padStart(2,'0') + value;
 }
 
-function khqr(amount,invoice){
-  let s='';
-  s+=tag('00','01');
-  s+=tag('01','12'); // dynamic
-  s+=tag('29', tag('00','ABA') + tag('01','1407908'));
-  s+=tag('52','5399');
-  s+=tag('53','840'); // USD
-  s+=tag('54',amount);
-  s+=tag('58','KH');
-  s+=tag('59','LOK POEUY');
-  s+=tag('62', tag('01',invoice));
-  s+= '6304';
-  return s + crc16(s);
+function khqr(amount, invoice){
+  let p = '';
+  p += tag('00','01');
+  p += tag('01','12'); // dynamic
+  p += tag('29',
+        tag('00','ABA') +
+        tag('01','1407908')
+      );
+  p += tag('52','5399');
+  p += tag('53','840'); // USD
+  p += tag('54',amount);
+  p += tag('58','KH');
+  p += tag('59','BARDS');
+  p += tag('62', tag('01',invoice));
+  p += '6304';
+  return p + crc16(p);
 }
-</script>
